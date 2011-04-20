aef44d52d7e7adec611dd71c22c8973399db2954
KAWACHI Takashi <tkawachi@gmail.com>
2011-04-21 01:32:41 +0900

    Added extern "C"

--- src/geohexv3.h
+++ src/geohexv3.h
@@ -1,5 +1,10 @@
 #ifndef GEOHEXV3_H__
 #define GEOHEXV3_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 struct geohexv3_zone {
 	int level;
 	char *code;
@@ -13,4 +18,9 @@ struct geohexv3_zone* geohexv3_zone_alloc(void);
 void geohexv3_zone_free(struct geohexv3_zone *zone);
 int geohexv3_get_zone_by_location(double lat, double lon, int level, struct geohexv3_zone *zone);
 int geohexv3_get_zone_by_code(const char *code, struct geohexv3_zone *zone);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
 #endif /* GEOHEXV3_H__ */
